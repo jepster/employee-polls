e45c895a2efe2df210e2b580c65ebd43c0f1078b
Peter Majmesku <majmesku@publicplan.de>
2022-07-15 09:00:29 +0200

    Better looking login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,25 +24,33 @@ const Login = ({ dispatch }) => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h2>Sign in</h2>
       <form>
         <label>User</label>
-        <Dropdown>
-          <Dropdown isOpen={state.dropdownOpen} toggle={toggle}>
-            <DropdownToggle caret>
-              {state.selectedUser}
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>Sylvester Stallone</DropdownItem>
-              <DropdownItem>Bruce Willis</DropdownItem>
-              <DropdownItem>Tom Cruise</DropdownItem>
-            </DropdownMenu>
+        <div className="row">
+          <Dropdown>
+            <Dropdown isOpen={state.dropdownOpen} toggle={toggle}>
+              <DropdownToggle caret>
+                {state.selectedUser}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Sylvester Stallone</DropdownItem>
+                <DropdownItem>Bruce Willis</DropdownItem>
+                <DropdownItem>Tom Cruise</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
           </Dropdown>
-        </Dropdown>
-        <label>Password</label>
+        </div>
+        <div className="row pt-3">
+          <label>Password (insert any you like)</label>
+        </div>
         <input type="password" name="password" />
-        <Button color="primary" onClick={setUserSignedIn}>Submit</Button>
+        <div className="row pt-3">
+          <div className="col-2">
+            <Button color="primary" onClick={setUserSignedIn}>Submit</Button>
+          </div>
+        </div>
       </form>
     </div>
   );
