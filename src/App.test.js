0dccea77651385eee29a3ce71ab0de20a252c11d
Peter Majmesku <majmesku@publicplan.de>
2022-07-21 16:33:25 +0200

    Login form can be rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import {getByText, render, screen} from '@testing-library/react';
 import App from './components/App';
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./store";
 import {Provider} from "react-redux";
+import Login from "./components/Login";
+import '@testing-library/jest-dom'
 
 describe("App", () => {
 
@@ -16,6 +18,21 @@ describe("App", () => {
       </Provider>
     );
     expect(component).toBeDefined();
+    expect(component).toMatchSnapshot();
+  });
+
+  it("check if we have login form", () => {
+    let component;
+    component = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Login/>
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(component).toBeDefined();
+    expect(component.getByText('User')).toBeInTheDocument();
+    expect(component.getByText('Password (insert any you like)')).toBeInTheDocument();
     expect(component).toMatchSnapshot();
   });
 
