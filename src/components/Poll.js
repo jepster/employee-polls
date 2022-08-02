266a63fb41400b3865b0d33e0e4b90f8b8675922
Peter Majmesku <majmesku@publicplan.de>
2022-08-02 11:10:26 +0200

    Able to count votes

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -10,9 +10,35 @@ const Poll = () => {
   const state = useSelector((state) => state);
 
   const pollId = useParams().id;
-  const poll = Object.values(state.polls.polls).find((poll) => {
-    return poll.id === pollId;
-  });
+
+  let votesFirstOption = 0;
+  let votesSecondOption = 0;
+  let poll;
+  for (let i = 0; i < Object.values(state.polls.polls).length; i++) {
+    poll = Object.values(state.polls.polls)[i];
+    if (poll.id === pollId) {
+      votesFirstOption += poll.votes_first_option;
+      votesSecondOption += poll.votes_second_option;
+
+      if (state.votes.votes !== undefined) {
+        for (let i = 0; i < Object.values(state.votes.votes).length; i++) {
+          let vote = Object.values(state.votes.votes)[i];
+          if (vote.poll_id === pollId && vote.option === 'first_option') {
+            votesFirstOption++;
+          }
+          if (vote.poll_id === pollId && vote.option === 'second_option') {
+            votesSecondOption++;
+          }
+        }
+      }
+    }
+
+    poll = {
+      ...poll,
+      votesFirstOption: votesFirstOption,
+      votesSecondOption: votesSecondOption,
+    }
+  }
 
   const [pollState, setPollState] = useState(poll);
   const dispatch = useDispatch();
@@ -43,11 +69,17 @@ const Poll = () => {
           <p>
             { (pollState && pollState.hasOwnProperty('firstOption')) ? pollState.firstOption : ''}
           </p>
+          <p>
+            Votes count: { pollState.votesFirstOption }
+          </p>
           <Button color="primary" onClick={() => dispatch(handleAddVote('first_option', pollState.id))}>Vote</Button>
         </div>
         <div className="col-md-6">
           <p>
             { (pollState && pollState.hasOwnProperty('secondOption')) ? pollState.secondOption : ''}
+          </p>
+          <p>
+            Votes count: { pollState.votesSecondOption }
           </p>
           <Button color="primary" onClick={() => dispatch(handleAddVote('second_option', pollState.id))}>Vote</Button>
         </div>
